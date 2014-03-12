232430e6461ce1a2682e3673b30bd7bf5834fcae
taesiri <taesiri@live.com>
2014-03-13 00:08:33 +0330

    Pipe Generator

---
+++ Assets/Scripts/PipeGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Assets.Scripts
+{
+    public class PipeGenerator : MonoBehaviour
+    {
+        public float Distance;
+        public int NumberOfPipes = 10;
+        public GameObject Pipe;
+        private GameObject[] _pipes;
+        private readonly Random _randomGenerator = new Random(DateTime.Now.Millisecond);
+
+
+        private void Start()
+        {
+            _pipes = new GameObject[NumberOfPipes];
+
+            for (int i = 0; i < NumberOfPipes; i++)
+            {
+                var newPipe = Instantiate(Pipe, Vector3.right*5 + Vector3.right*i*Distance, Quaternion.identity) as GameObject;
+
+                if (newPipe != null)
+                {
+                    var pscript = newPipe.GetComponent<PipeScript>();
+
+                    pscript.Distance = Distance;
+
+                    pscript.NumberOfPipes = NumberOfPipes;
+                    pscript.SetYOffset(_randomGenerator.Next(1, 780)/100f - 3.8f);
+
+                    _pipes[i] = newPipe;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PipeScript.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Assets.Scripts
+{
+    public class PipeScript : MonoBehaviour
+    {
+        private readonly Random _randomGenerator = new Random(DateTime.Now.Millisecond);
+
+        public float Speed = 10;
+
+        public float Distance;
+        public int NumberOfPipes = 10;
+
+        public float XOffset = -4;
+
+        public void SetYOffset(float yOffset)
+        {
+            transform.position = new Vector3(transform.position.x, yOffset, transform.position.z);
+        }
+
+        public void RandomYOffset()
+        {
+            var y = _randomGenerator.Next(1, 780)/100f - 3.8f;
+            transform.position = new Vector3(transform.position.x, y, transform.position.z);
+        }
+
+        public void Update()
+        {
+            transform.position += Vector3.left*Time.deltaTime*Speed;
+
+            if (transform.position.x < -4)
+            {
+                transform.position += Vector3.right*NumberOfPipes*Distance;
+                RandomYOffset();
+            }
+        }
+    }
+}
